9840ac24807c0629a4da7a997a6b9c81a948dfc1
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2024-11-29 21:55:29 +0700

    check commit search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
 const navPC = document.querySelector("#header__nav-list--pc");
-console.log(navPC);
 const navMobile = document.querySelector("#header__nav-list--mobile");
 navMobile.innerHTML = navPC.innerHTML;
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,19 +1,20 @@
 const listImage = document.querySelector(".slider-image-list");
-const imgs = document.querySelectorAll(".slider-item"); 
+const imgs = document.querySelectorAll(".slider-item");
 const btnLeft = document.querySelector(".btn-left");
 const btnRight = document.querySelector(".btn-right");
 const length = imgs.length;
-let current = 0;
+let current= 0;
 
 // Hàm cập nhật slider
 function updateSlider() {
   const width = imgs[0].offsetWidth; // Lấy chiều rộng mỗi lần cập nhật để đảm bảo tính linh hoạt
   listImage.style.transform = `translateX(${-width * current}px)`;
   listImage.style.transition = "transform 0.5s ease-in-out";
+  console.log(width);
 }
 
 // Tự động chuyển ảnh
-const autoSlide = setInterval(() => {
+const autoSlider = setInterval(() => {
   if (current === length - 1) {
     current = 0; // Quay lại ảnh đầu tiên
   } else {
@@ -24,8 +25,8 @@ const autoSlide = setInterval(() => {
 
 // Sự kiện Click nút Next
 btnRight.addEventListener("click", () => {
-  clearInterval(autoSlide); // Dừng tự động chuyển ảnh khi người dùng nhấn
-  if (current === length - 1) {
+  clearInterval(autoSlider); // Dừng tự động chuyển ảnh khi người dùng click vào
+  if(current === length - 1) {
     current = 0;
   } else {
     current++;
@@ -35,8 +36,8 @@ btnRight.addEventListener("click", () => {
 
 // Sự kiện Click nút Previous
 btnLeft.addEventListener("click", () => {
-  clearInterval(autoSlide); // Dừng tự động chuyển ảnh khi người dùng nhấn
-  if (current === 0) {
+  clearInterval(autoSlider); // Dừng tự động chuyển ảnh khi người dùng click vào
+  if(current === 0) {
     current = length - 1;
   } else {
     current--;
